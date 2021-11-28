91c1117705c608aaccad6b481dd04f34360c3a5b
Anna-Mogilevskaya <mogilevs.anna@gmail.com>
2021-11-29 00:42:46 +0200

    task 1
    task 1

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,44 @@
+import SimpleLightbox from "simplelightbox";
+import 'simplelightbox/dist/simple-lightbox.min.css';
+
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
 console.log(galleryItems);
+
+const galleryContainer = document.querySelector('.gallery');
+const galleryMarkup = createGalletyItemsMarkup(galleryItems);
+
+galleryContainer.insertAdjacentHTML('beforeend', galleryMarkup);
+
+function createGalletyItemsMarkup(galleryItems) {
+  return galleryItems
+    .map(({ preview, original, description }) => {
+        return `
+     <li> 
+        <a class="gallery__item"
+            href="${original}">
+            <img class="gallery__image"
+            src="${preview}" 
+            alt="${description}" />
+        </a>
+     </li>
+    `;
+    })
+    .join('');
+}
+
+galleryContainer.addEventListener('click', onShowGalleryModal);
+
+function onShowGalleryModal(event) {
+    event.preventDefault();
+
+     if (event.target.nodeName !== 'IMG') {
+    return;
+    };
+    
+    const lightbox = new SimpleLightbox('.gallery a', { captionsData: 'alt', captionDelay: 250, });
+}
+
+
