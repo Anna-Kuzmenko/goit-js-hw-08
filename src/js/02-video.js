36a8b7afc38b34bf0768b658ca8f1330a57db4c2
Anna-Mogilevskaya <mogilevs.anna@gmail.com>
2021-11-29 03:10:43 +0200

    task 2
    task 2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,29 @@
+const throttle = require('lodash.throttle');
 
+const iframe = document.querySelector('iframe');
+const player = new Vimeo.Player(iframe);
+
+const saveVideoTime = 'videoplayer-current-time';
+
+player.on('timeupdate', throttle(playOn, 500));
+
+function playOn({ seconds }) {
+    localStorage.setItem(saveVideoTime, seconds);
+}
+
+player.setCurrentTime(localStorage.getItem(saveVideoTime))
+    .catch(function (error) {
+    switch (error.name) {
+        case 'RangeError':
+            console.log('the time was less than 0 or greater than the video’s duration')
+            break;
+
+        default:
+            console.log('some other error occurred')
+            break;
+    }
+});
+
+
+
+console.log(localStorage.getItem(saveVideoTime));
