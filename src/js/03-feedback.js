6747ee96313ac7f8baf6be390c76436857c78a12
Anna-Mogilevskaya <mogilevs.anna@gmail.com>
2021-11-29 05:06:39 +0200

    task-3
    task-3

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,49 @@
+const throttle = require('lodash.throttle');
+
+const feedbackForm = document.querySelector('.feedback-form');
+const email = feedbackForm.elements.email;
+const message = feedbackForm.elements.message;
+
+const savedFormData = 'feedback-form-state';
+
+const userData = {};
+
+if (localStorage[savedFormData]) {
+
+    const LocalData = JSON.parse(localStorage[savedFormData]);
+
+    email.value = LocalData.email;
+    message.value = LocalData.message;
+}
+
+feedbackForm.addEventListener('input', throttle(onFormInput, 500));
+
+function onFormInput() {
+    const formData = new FormData(feedbackForm);
+
+    formData.forEach((value, name) => {
+
+        userData[name] = value;
+        localStorage.setItem(savedFormData, JSON.stringify(userData));
+
+    });
+}
+
+feedbackForm.addEventListener('submit', onFormSubmit);
+
+function onFormSubmit(event) {
+
+    event.preventDefault();
+
+    const LocalData = JSON.parse(localStorage[savedFormData]);
+    console.log(LocalData);
+
+    localStorage.clear();
+
+     feedbackForm.reset();
+}
+
+
+
+
 
